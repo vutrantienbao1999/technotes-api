db9a0d1494b13b30b413618b8a31d74147e63005
Vu Tran Tien Bao <vutrantienbao@gmail.com>
2023-03-13 22:25:02 +0700

    Add stricQuery to false

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.DATABASE_URI)
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DATABASE_URI)
   } catch (err) {
     console.log(err)
   }
